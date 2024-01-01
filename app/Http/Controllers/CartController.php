2f39d3eff52cc7180e88fe89e7f653fe397998a2
Emmanuel <emmanuel@emmanuel-VivoBook-ASUSLaptop-X409FA-X409FA>
2024-01-01 03:55:19 +0100

    cart page functional, awaiting refactoring

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -12,7 +12,7 @@ class CartController extends Controller
 {
     public function index(){
         $user = Auth::user();
-        return view('cart', ["cart" => $user->cart]);
+        return view('cart', ["carts" => $user->cart]);
     }
     //
     public function create($id){
@@ -24,7 +24,14 @@ class CartController extends Controller
         if($product && $user){
             error_log($product->price);
             $cart = $user->cart;
-            $cart[] = ['id' => "$product->id", 'price' => "$product->price"];
+            //I had to do add the contents individually cos i had tons of issues when I tried to add the entire product .... the easier route is probably the better route.
+            $cart[] = ['id' => "$product->id",
+                       'price' => "$product->price", 
+                       'img' => "$product->url", 
+                       'name' => "$product->name", 
+                       'details' => "$product->details", 
+                       'instock' => "$product->instock"
+                    ];
             $user->cart = $cart;
             $user->save();
             return redirect('/');
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,6 +1,27 @@
 @extends('layouts.head')
 @section('content')
     <div class="cart">
-        {{ Auth::user()->name }}
+        <div class="container">
+            @foreach($carts as $cart)
+                <div>
+                    <div class="image">
+                        <img src="{{$cart['img']}}" alt="">
+                    </div>
+                    <div class="cont">
+                        <h1>{{$cart['name']}} </h1>
+                        <h3>{{ $cart['details'] }}</h3>
+                        <p>
+                            @if($cart['instock'] > 10)
+                                instock
+                            @elseif($cart['instock'] <= 10 && $cart['instock'] > 0)
+                                limited
+                            @else
+                                out of stock
+                            @endif
+                        </p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
